8287690711c5b7d8d780ddce8780367add8c5cd8
parth1099 <parth.infynno@gmail.com>
2022-08-16 11:39:55 +0530

    16-08

--- src/Component/Scorecard.jsx
+++ src/Component/Scorecard.jsx
@@ -4,8 +4,7 @@ import axios from "axios";
 import { IoIosArrowDropdownCircle } from "react-icons/io";
 import { base_url, api_token, all_include } from "../Config.js";
 import Dropscore from "./Dropscore.jsx";
-import { Audio } from  'react-loader-spinner'
-
+import { RotatingLines } from "react-loader-spinner";
 
 export default function Scorecard() {
   let { id } = useParams();
@@ -20,11 +19,8 @@ export default function Scorecard() {
 
   const [localbow, setLocalBow] = useState([]);
   const [visitorbow, setVisitorBow] = useState([]);
-  const [localextra,setLocalExtra] = useState([]);
-  const [visitorextra,setVisitorExtra]= useState([]);
-
-
-
+  const [localextra, setLocalExtra] = useState([]);
+  const [visitorextra, setVisitorExtra] = useState([]);
 
   async function fetchscore() {
     const url = base_url + "/" + id + api_token + "&" + all_include;
@@ -33,7 +29,6 @@ export default function Scorecard() {
     if (status === 200) {
       setScore(data.data);
     }
-    
 
     // LocalTeam Score
     const [localsc] = data?.data?.runs.filter((datanew) => {
@@ -63,139 +58,155 @@ export default function Scorecard() {
 
     //VisitorBat Data
     const visitorbatsc = data?.data?.batting.filter((visitorsc) => {
-      if(data?.data?.visitorteam.id === visitorsc?.team_id )
-      {
-          return visitorsc;
+      if (data?.data?.visitorteam.id === visitorsc?.team_id) {
+        return visitorsc;
       }
-    })
+    });
     setVisitorBat(visitorbatsc);
     // console.log(score,"score")
 
-  // LocalTeam Bowler Filter
-  const localbowler = data?.data?.bowling.filter((localbw) => {
-      if(data?.data?.visitorteam.id === localbw?.team_id)
-      {
+    // LocalTeam Bowler Filter
+    const localbowler = data?.data?.bowling.filter((localbw) => {
+      if (data?.data?.visitorteam.id === localbw?.team_id) {
         return localbw;
       }
-    })
+    });
     setLocalBow(localbowler);
 
-
-    // VisitorTeam Bowler Filter 
-    const visitorbowler = data?.data?.bowling.filter((visitorbw => {
-      if(data?.data?.localteam.id === visitorbw?.team_id)
-      {
+    // VisitorTeam Bowler Filter
+    const visitorbowler = data?.data?.bowling.filter((visitorbw) => {
+      if (data?.data?.localteam.id === visitorbw?.team_id) {
         return visitorbw;
       }
-    }))
+    });
     setVisitorBow(visitorbowler);
 
-    //LocalTeam Extra : 
-    const tlocalextra = data?.data?.scoreboards?.filter((ex) =>{
-      if(data?.data?.localteam.id === ex?.team_id)
-      { 
+    //LocalTeam Extra :
+    const tlocalextra = data?.data?.scoreboards?.filter((ex) => {
+      if (data?.data?.localteam.id === ex?.team_id) {
         return ex;
       }
-    })
-    setLocalExtra(tlocalextra[0]) ;
+    });
+    setLocalExtra(tlocalextra[0]);
 
     //Visitor Team Extra
     const tvisitorextra = data?.data?.scoreboards?.filter((ex) => {
-      if(data?.data?.visitorteam.id === ex?.team_id)
-      {
+      if (data?.data?.visitorteam.id === ex?.team_id) {
         return ex;
       }
-    })
+    });
     setVisitorExtra(tvisitorextra[0]);
-
-
-    
-
   }
-  console.log("Local Team Extra Data : ", localextra)
-  console.log("Visitor Team Extra Data : ", visitorextra)
- 
+  console.log("Local Team Extra Data : ", localextra);
+  console.log("Visitor Team Extra Data : ", visitorextra);
+
   useEffect(() => {
     fetchscore();
-
   }, []);
-  
+
   return (
     <>
-    
-      <section className="flex justify-center">
-        <div className="flex-col justify-center">
-          <div className="w-[700px] mt-[30px] flex flex-col gap-4">
-            <div className="score-main flex justify-between items-center">
-              <div>
-                <div className="text-[11px] flex">
-                  <p className="px-[5px] py-[3px] bg-[#EB5764] rounded-[2px] text-white">
-                    {score?.localteam.code}
-                  </p>
+      {score ? (
+        <>
+          <section className="flex justify-center">
+            <div className="flex-col justify-center">
+              <div className="w-[700px] mt-[30px] flex flex-col gap-4">
+                <div className="score-main flex justify-between items-center">
+                  <div>
+                    <div className="text-[11px] flex">
+                      <p className="px-[5px] py-[3px] bg-[#EB5764] rounded-[2px] text-white">
+                        {score?.localteam.code}
+                      </p>
+                    </div>
+                    <div className="flex  gap-1">
+                      <h1 className="text-[28px] font-[600] tracking-[0.14px] leading-[42px]">
+                        {localscore?.score}/{localscore?.wickets}
+                        {/* {score?.runs[0].score}/{score?.runs[0].wickets} */}
+                      </h1>
+                      <p className="text-[12px] flex items-end font-[600] leadning-[1.33] text-[#787878]">
+                        ({localscore?.overs})
+                      </p>
+                    </div>
+                  </div>
+                  <div className="flex flex-col justify-center items-center">
+                    <div className="h-[20px] w-[2px] bg-[#e6e6e6] justify-center items-center "></div>
+                    <div className="h-[30px] w-[30px] justify-center items-center flex bg-[#e6e6e6] rounded-[50%] text-white text-[15px]">
+                      v
+                    </div>
+                    <div className="h-[20px] w-[2px] bg-[#e6e6e6] flex justify-center items-center "></div>
+                  </div>
+                  <div>
+                    <div className="flex justify-end text-[11px]">
+                      <p className="px-[5px] py-[3px] bg-[#53A784] rounded-[2px] text-white">
+                        {score?.visitorteam.code}
+                      </p>
+                    </div>
+                    <div className="flex  gap-1">
+                      <p className="text-[12px] font-[600] text-[#787878] leading-[1.33] flex items-end">
+                        ({visitorscore?.overs})
+                      </p>
+                      <h1 className="text-[28px] leading-[42px] font-[600] tracking-[0.14px]">
+                        {visitorscore?.score}/{visitorscore?.wickets}
+                      </h1>
+                    </div>
+                  </div>
                 </div>
-                <div className="flex  gap-1">
-                  <h1 className="text-[28px] font-[600] tracking-[0.14px] leading-[42px]">
-                    {localscore?.score}/{localscore?.wickets}
-                    {/* {score?.runs[0].score}/{score?.runs[0].wickets} */}
-                  </h1>
-                  <p className="text-[12px] flex items-end font-[600] leadning-[1.33] text-[#787878]">
-                    ({localscore?.overs})
-                  </p>
+                <div className="text-center tracking-tighter-[0.28px] text-[14px] font-[600]">
+                  {score?.note}
                 </div>
-              </div>
-              <div className="flex flex-col justify-center items-center">
-                <div className="h-[20px] w-[2px] bg-[#e6e6e6] justify-center items-center "></div>
-                <div className="h-[30px] w-[30px] justify-center items-center flex bg-[#e6e6e6] rounded-[50%] text-white text-[15px]">
-                  v
-                </div>
-                <div className="h-[20px] w-[2px] bg-[#e6e6e6] flex justify-center items-center "></div>
-              </div>
-              <div>
-                <div className="flex justify-end text-[11px]">
-                  <p className="px-[5px] py-[3px] bg-[#53A784] rounded-[2px] text-white">
-                    {score?.visitorteam.code}
-                  </p>
-                </div>
-                <div className="flex  gap-1">
-                  <p className="text-[12px] font-[600] text-[#787878] leading-[1.33] flex items-end">
-                    ({visitorscore?.overs})
-                  </p>
-                  <h1 className="text-[28px] leading-[42px] font-[600] tracking-[0.14px]">
-                    {visitorscore?.score}/{visitorscore?.wickets}
-                  </h1>
-                </div>
-              </div>
-            </div>
-            <div className="text-center tracking-tighter-[0.28px] text-[14px] font-[600]">
-              {score?.note}
-            </div>
 
-            <section className="flex justify-between  border-t-[1px] border-b-[1px] border-solid border-[#e6e6e6] py-[14px] ">
-              <div className="text-[14px] text-[#787878] px-[35px] ">
-                <p>Fantasy</p>
-              </div>
-              <div className="text-[14px] text-[#787878] px-[35px] ">
-                <p>Info</p>
-              </div>
-              <div className="text-[14px] text-[#787878] px-[35px] ">
-                <p>Live</p>
-              </div>
-              <div className="text-[14px] text-[#000] px-[33px] font-[600] ">
-                <p>Scorecard</p>
-              </div>
-              <div className="text-[14px] text-[#787878] px-[35px] ">
-                <p>Squad</p>
-              </div>
-              {/* <div className="text-[14px] text-[#787878] px-[35px] ">
+                <section className="flex justify-between  border-t-[1px] border-b-[1px] border-solid border-[#e6e6e6] py-[14px] ">
+                  <div className="text-[14px] text-[#787878] px-[35px] ">
+                    <p>Fantasy</p>
+                  </div>
+                  <div className="text-[14px] text-[#787878] px-[35px] ">
+                    <p>Info</p>
+                  </div>
+                  <div className="text-[14px] text-[#787878] px-[35px] ">
+                    <p>Live</p>
+                  </div>
+                  <div className="text-[14px] text-[#000] px-[33px] font-[600] ">
+                    <p>Scorecard</p>
+                  </div>
+                  <div className="text-[14px] text-[#787878] px-[35px] ">
+                    <p>Squad</p>
+                  </div>
+                  {/* <div className="text-[14px] text-[#787878] px-[35px] ">
                 <p>Highlights</p>
               </div> */}
-            </section>
-          </div>
+                </section>
+              </div>
 
-          <Dropscore teamcode ={score?.localteam.code} score={score} teamscore={localscore} Bat={localbat} Bowl={localbow} Extra={localextra} />
-          <Dropscore teamcode ={score?.visitorteam.code} score={score} teamscore={visitorscore} Bat={visitorbat} Bowl={visitorbow} Extra={visitorextra}/>
+              <Dropscore
+                teamcode={score?.localteam.code}
+                score={score}
+                teamscore={localscore}
+                Bat={localbat}
+                Bowl={localbow}
+                Extra={localextra}
+              />
+              <Dropscore
+                teamcode={score?.visitorteam.code}
+                score={score}
+                teamscore={visitorscore}
+                Bat={visitorbat}
+                Bowl={visitorbow}
+                Extra={visitorextra}
+              />
+            </div>
+          </section>
+        </>
+      ) : (
+        <div className="h-[300px] flex justify-center items-center">
+          <RotatingLines
+            strokeColor="grey"
+            strokeWidth="3"
+            animationDuration="1.75"
+            width="46"
+            visible={true}
+          />
         </div>
-      </section>
+      )}
     </>
   );
 }
--- src/Component/Slider.jsx
+++ src/Component/Slider.jsx
@@ -5,131 +5,125 @@ import Slider from "react-slick";
 import { base_url, api_token, all_include } from "../Config.js";
 import axios from "axios";
 import Fixed from "./Fixed";
-import { Oval } from  'react-loader-spinner'
+import { Oval } from "react-loader-spinner";
 
-
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 4,
-    nextArrow: <SampleNextArrow />,
-    prevArrow: <SamplePrevArrow />,
-    appendDots: (dots) => (
-      <div style={{}}>
-        <ul
-          style={{
-            position: "relative",
-            float: "right",
-            top: "-315px",
-            right: "20px",
-          }}
-        >
-          {dots}
-        </ul>
-      </div>
-    ),
-    customPaging: (dots) => (
-      <div
+const settings = {
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 3,
+  slidesToScroll: 4,
+  nextArrow: <SampleNextArrow />,
+  prevArrow: <SamplePrevArrow />,
+  appendDots: (dots) => (
+    <div style={{}}>
+      <ul
         style={{
-          fontSize: "60px",
-          color: "white",
+          position: "relative",
+          float: "right",
+          top: "-315px",
+          right: "20px",
         }}
-      ></div>
-    ),
-  };
-
-  function SampleNextArrow(props) {
-    const { className, style, onClick } = props;
-    return (
-      <div
-        className={className}
-        style={{
-          ...style,
-          display: "block",
-          background: "white",
-          height: "48px",
-          top: "95px",
-
-          width: "48px",
-          overflow: "hidden",
-          borderRadius: "50%",
-          zIndex: "2",
-        }}
-        onClick={onClick}
       >
-        <HiArrowRight
-          fill="red"
-          size={24}
-          className="relative top-[-8px] left-[12px]"
-        />
-      </div>
-    );
-  }
+        {dots}
+      </ul>
+    </div>
+  ),
+  customPaging: (dots) => (
+    <div
+      style={{
+        fontSize: "60px",
+        color: "white",
+      }}
+    ></div>
+  ),
+};
 
-  function SamplePrevArrow(props) {
-    const { className, style, onClick } = props;
-    return (
-      <div
-        className={className}
-        style={{
-          ...style,
-          display: "block",
-          background: "white",
-          top: "95px",
-          height: "48px",
-          width: "48px",
-          overflow: "hidden",
-          borderRadius: "50%",
-          zIndex: "2",
-        }}
-        onClick={onClick}
-      >
-        <HiArrowLeft
-          fill="red"
-          size={24}
-          className="relative top-[-8px] left-[12px]"
-        />
-      </div>
-    );
-  }
+function SampleNextArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{
+        ...style,
+        display: "block",
+        background: "white",
+        height: "48px",
+        top: "95px",
+
+        width: "48px",
+        overflow: "hidden",
+        borderRadius: "50%",
+        zIndex: "2",
+      }}
+      onClick={onClick}
+    >
+      <HiArrowRight
+        fill="red"
+        size={24}
+        className="relative top-[-8px] left-[12px]"
+      />
+    </div>
+  );
+}
+
+function SamplePrevArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{
+        ...style,
+        display: "block",
+        background: "white",
+        top: "95px",
+        height: "48px",
+        width: "48px",
+        overflow: "hidden",
+        borderRadius: "50%",
+        zIndex: "2",
+      }}
+      onClick={onClick}
+    >
+      <HiArrowLeft
+        fill="red"
+        size={24}
+        className="relative top-[-8px] left-[12px]"
+      />
+    </div>
+  );
+}
 
 export default function Slidernew() {
   const [matchdata, setMatchData] = useState();
   const [matchfinished, setFinished] = useState(null);
-  const [upcoming,setUpcoming] = useState(null);
+  const [upcoming, setUpcoming] = useState(null);
 
   async function fetchMatchData() {
     const url = `${base_url}${api_token}&include=localteam,visitorteam,season,runs,league`;
-    const {status,data} = await axios.get(url);
+    const { status, data } = await axios.get(url);
     // const data = res.data;
     // console.log(data.data.splice(10,20));
-    if(status === 200)
-    {
-      setMatchData(data.data.slice(45,55));
-      
+    if (status === 200) {
+      setMatchData(data.data.slice(45, 55));
     }
 
     // To Matched Finish
     const finish = data.data.filter((datafinish) => {
-      if(datafinish.live == false ){
+      if (datafinish.live == false) {
         return datafinish;
       }
     });
     setFinished(finish);
     // console.log(finish,"Finish");
 
-
     const upcomingone = data.data.filter((dataupcome) => {
-      if(dataupcome.status ===  "Aban." )
-      {
+      if (dataupcome.status === "Aban.") {
         return dataupcome;
       }
     });
     setUpcoming(upcomingone);
     // console.log(upcomingone,"Upcoming Data");
-  
   }
 
   useEffect(() => {
@@ -138,43 +132,43 @@ export default function Slidernew() {
 
   return (
     <>
-      {matchdata ?  (<>
-      <section className="h-[166px] bg-nav-rgba flex justify-center mb-[150px]">
-        <div className="w-[1100px] ml-[55px] flex flex-col gap-[16px] px-[16px] pt-[20px] ">
-          <p className="text-white text-[20px] font-[600]">Featured Matches</p>
-          <div className="">
-            <Slider {...settings}>
-            
-              {matchdata?.map((matchdata) =>{
-                return(
-                  <>
-              <Card datamatches ={matchdata} />
+      {matchdata ? (
+        <>
+          <section className="h-[166px] bg-nav-rgba flex justify-center mb-[150px]">
+            <div className="w-[1100px] ml-[55px] flex flex-col gap-[16px] px-[16px] pt-[20px] ">
+              <p className="text-white text-[20px] font-[600]">
+                Featured Matches
+              </p>
+              <div className="">
+                <Slider {...settings}>
+                  {matchdata?.map((matchdata) => {
+                    return (
+                      <>
+                        <Card datamatches={matchdata} />
+                      </>
+                    );
+                  })}
+                </Slider>
+              </div>
+            </div>
+          </section>
 
-                  </>
-                );
-              })}
-            </Slider>
-          </div>
+          <Fixed title="Finished Matches" data={matchfinished} />
+          <Fixed title="Upcoming Matches" data={upcoming} />
+        </>
+      ) : (
+        <div className="flex justify-center items-center h-[500px]">
+          <Oval
+            height="50"
+            width="100"
+            radius="9"
+            color="black"
+            ariaLabel="three-dots-loading"
+            wrapperStyle
+            wrapperClass
+          />
         </div>
-      </section>
-    
-      <Fixed title ="Finished Matches" data={matchfinished} />
-      <Fixed title="Upcoming Matches" data={upcoming}/>
-            </>)
-
-
-      : <div className="flex justify-center items-center h-[500px]"><Oval
-    height = "80"
-    width = "80"
-    radius = "9"
-    color = 'grey'
-    ariaLabel = 'three-dots-loading'     
-    wrapperStyle
-    wrapperClass
-  /></div>
-            }
-          
-      
+      )}
     </>
   );
 }
